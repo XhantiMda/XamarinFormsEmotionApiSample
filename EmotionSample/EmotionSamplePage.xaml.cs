ec8238a5f35c91bdacf67213b3616ca9d5df1066
Xhanti Mda <xhantimda@hotmail.com>
2017-11-12 18:12:32 +0200

    Initial commit

---
+++ EmotionSample/EmotionSamplePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using EmotionSample.Services;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace EmotionSample
+{
+    public partial class EmotionSamplePage : ContentPage
+    {
+        MediaFile _file;
+
+        public EmotionSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        async void ComputeImage(object sender, System.EventArgs e)
+        {
+            if (_file == null)
+                return;
+
+            var result = await EmotionService.GetAverageEmotionAsync(_file.GetStream());
+
+            await DisplayAlert("Message", result, "Ok");
+        }
+
+        async void CaptureImage(object sender, System.EventArgs e)
+        {
+            _file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Small,
+                Name = "helloEmotions.jpg",
+                Directory = "EmotionSample"
+            });
+
+            if (_file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() => (_file.GetStream()));
+        }
+    }
+}
---
+++ EmotionSample/Services/EmotionService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using System.Reflection;
+
+namespace EmotionSample.Services
+{
+    public static class EmotionService
+    {
+        private static async Task<Emotion[]> AnalyzeImageAsync(Stream stream)
+        {
+            var emotionClient = new EmotionServiceClient("40d5cb0944544b6689f3eb27d499099a");
+            return await emotionClient.RecognizeAsync(stream);
+        } 
+
+        public static async Task<string> GetAverageEmotionAsync(Stream stream)
+        {
+            var emotions = await AnalyzeImageAsync(stream);
+
+            if (emotions == null || !emotions.Any())
+            {
+                return "No emotions detected";
+            }
+
+           return GetHighEmotionScore(emotions.FirstOrDefault());
+        }
+
+        private static string GetHighEmotionScore(Emotion emotion)
+        {
+            var properties = emotion.Scores.GetType().GetRuntimeProperties();
+
+            return properties.OrderByDescending(prop => prop.GetValue(emotion.Scores))
+                             .Select(prop => 
+                                     $"Emotion : {prop.Name} \n Score : {(float)prop.GetValue(emotion.Scores) * 100}")
+                             .FirstOrDefault();
+        }
+    }
+}
